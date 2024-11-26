9e438eaa71e9013571325d684146202cb19b2776
berry8848 <46173232+berry8848@users.noreply.github.com>
2024-11-26 18:35:54 +0900

    Djangoの秘密キーの隠ぺい

--- src/backend/djangoApiApp/djangoApiApp/settings.py
+++ src/backend/djangoApiApp/djangoApiApp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from .settings_local import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dh^yy_8uxxrifq#mn)^d$1e3rk$4njj7vxb_8ac1mk3v=-wqt1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
