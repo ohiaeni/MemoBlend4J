b7dd369cbaa10f67c7a8787903914a1cfcc196a0
yoshidakenji <63270478+ohiaeni@users.noreply.github.com>
2024-12-26 17:58:24 +0900

    spring-backendの新規作成

---
+++ src/spring-backend/system-common/src/main/java/com/memoblend/systemcommon/SystemCommonApplication.java
@@ -0,0 +1,13 @@
+package com.memoblend.systemcommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemCommonApplication.class, args);
+	}
+
+}
