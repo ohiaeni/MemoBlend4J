d1a81ff0362e0b49c295efaf8ccf358100cdfea0
yoshidakenji <63270478+ohiaeni@users.noreply.github.com>
2025-01-09 21:48:04 +0900

    日記のドメインモデルの追加

--- src/spring-backend/application-core/src/main/java/com/memoblend/applicationcore/diary/Diary.java
+++ src/spring-backend/application-core/src/main/java/com/memoblend/applicationcore/diary/Diary.java
@@ -1,8 +1,16 @@
 package com.memoblend.applicationcore.diary;
 
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 /**
  * 日記のドメインモデルです。
  */
+@Data
+@AllArgsConstructor
 public class Diary {
-
+  private LocalDate date;
+  private String title;
+  private String content;
 }
