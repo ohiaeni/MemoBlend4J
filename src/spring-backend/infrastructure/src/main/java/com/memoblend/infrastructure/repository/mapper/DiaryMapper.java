6fa3a3944830bb8ea474b1d45cbc0095ab254949
yoshidakenji <63270478+ohiaeni@users.noreply.github.com>
2025-01-12 14:23:52 +0900

    リポジトリからマッパーのメソッドを呼び出すように変更

--- src/spring-backend/infrastructure/src/main/java/com/memoblend/infrastructure/repository/mapper/DiaryMapper.java
+++ src/spring-backend/infrastructure/src/main/java/com/memoblend/infrastructure/repository/mapper/DiaryMapper.java
@@ -9,19 +9,20 @@ import com.memoblend.applicationcore.diary.Diary;
  */
 @Mapper
 public interface DiaryMapper {
-    /**
-     * 日付を指定して、
-     * {@link Diary} を取得します。
-     * 
-     * @param date 日記を作成した日付。
-     * @return 条件に合う日記。
-     */
-    public Diary findByDate(LocalDate date);
 
-    /**
-     * 日記を追加します。
-     * 
-     * @param diary 追加する日記。
-     */
-    public void add(Diary diary);
+  /**
+   * 日付を指定して、
+   * {@link Diary} を取得します。
+   * 
+   * @param date 日記を作成した日付。
+   * @return 条件に合う日記。
+   */
+  public Diary findByDate(LocalDate date);
+
+  /**
+   * 日記を追加します。
+   * 
+   * @param diary 追加する日記。
+   */
+  public void add(Diary diary);
 }
