98042833c7dfca275e1c68a664f7e03e93467809
yoshidakenji <63270478+ohiaeni@users.noreply.github.com>
2025-01-31 10:56:48 +0900

    日記の更新に関わるdtoとマッパーの追加

--- backend/web/src/main/java/com/memoblend/web/controller/dto/diary/PutDiaryRequest.java
+++ backend/web/src/main/java/com/memoblend/web/controller/dto/diary/PutDiaryRequest.java
@@ -1,8 +1,19 @@
 package com.memoblend.web.controller.dto.diary;
 
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 /**
  * 日記の更新リクエストクラスです。
  */
+@Data
+@AllArgsConstructor
 public class PutDiaryRequest {
-
+    private long id;
+    private long userId;
+    private String title;
+    private String content;
+    private LocalDate date;
 }
---
+++ backend/web/src/main/java/com/memoblend/web/controller/dto/mapper/PutDiaryRequestMapper.java
@@ -0,0 +1,26 @@
+package com.memoblend.web.controller.dto.mapper;
+
+import com.memoblend.applicationcore.diary.Diary;
+import com.memoblend.web.controller.dto.diary.PutDiaryRequest;
+
+/**
+ * {@link PutDiaryRequest} から {@link Diary} への変換を行うクラスです。
+ */
+public class PutDiaryRequestMapper {
+
+    /**
+     * {@link PutDiaryRequest} から {@link Diary} へ変換します。
+     * 
+     * @param request 日記の更新リクエスト。
+     * @return 日記。
+     */
+    public static Diary convert(PutDiaryRequest request) {
+        Diary diary = new Diary(
+                request.getId(),
+                request.getUserId(),
+                request.getTitle(),
+                request.getContent(),
+                request.getDate());
+        return diary;
+    }
+}
