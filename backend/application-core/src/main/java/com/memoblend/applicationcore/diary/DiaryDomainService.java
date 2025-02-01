b9a8cb728d41a785449f389bb563dd5e80405ac0
yoshidakenji <63270478+ohiaeni@users.noreply.github.com>
2025-02-01 15:15:48 +0900

    日記のドメインサービスに指定した日付の日記が存在するかを取得するメソッドを追加

--- backend/application-core/src/main/java/com/memoblend/applicationcore/diary/DiaryDomainService.java
+++ backend/application-core/src/main/java/com/memoblend/applicationcore/diary/DiaryDomainService.java
@@ -1,8 +1,23 @@
 package com.memoblend.applicationcore.diary;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
+
 /**
  * 日記のドメインサービスです。
  */
+@AllArgsConstructor
 public class DiaryDomainService {
+  @Autowired
+  private DiaryRepository diaryRepository;
 
+  /**
+   * 日記が存在するかどうかを判定します。
+   * 
+   * @param diary 日記。
+   * @return 日記が存在する場合は true、存在しない場合は false。
+   */
+  public boolean isExistDiary(Diary diary) {
+    return diaryRepository.findByDate(diary.getDate()) != null;
+  }
 }
