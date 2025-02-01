7ad61e17a2b87107688fb97335fdf999e5df1325
yoshidakenji <63270478+ohiaeni@users.noreply.github.com>
2025-02-01 15:12:28 +0900

    longとLocalDateのコンバータークラスにおいて、非検査例外をキャッチしないように修正

--- backend/system-common/src/main/java/com/memoblend/systemcommon/util/LocalDateConverter.java
+++ backend/system-common/src/main/java/com/memoblend/systemcommon/util/LocalDateConverter.java
@@ -2,7 +2,6 @@ package com.memoblend.systemcommon.util;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 /**
  * 日付の変換を行うクラスです。
@@ -16,13 +15,9 @@ public class LocalDateConverter {
    * @return LocalDate型の日付
    */
   public static LocalDate convert(long date) {
-    try {
-      String dateString = String.valueOf(date);
-      DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
-      LocalDate localDate = LocalDate.parse(dateString, formatter);
-      return localDate;
-    } catch (DateTimeParseException e) {
-      throw new IllegalArgumentException("日付の形式が正しくありません: " + date, e);
-    }
+    String dateString = String.valueOf(date);
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+    LocalDate localDate = LocalDate.parse(dateString, formatter);
+    return localDate;
   }
 }
