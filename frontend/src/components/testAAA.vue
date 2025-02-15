9fa98b38de05780f72b631234bb66752501bee3d
yoshidakenji <63270478+ohiaeni@users.noreply.github.com>
2025-02-15 15:17:26 +0900

    フロントエンドの環境を削除

--- frontend/src/components/testAAA.vue
+++
@@ -1,9 +0,0 @@
-<template>
-<img alt="Vue logo" class="m-auto" src="@/assets/logo.svg" width="125" height="125" />
-    <div class="flex flex-col justify-center text-center">
-      <div class="bg-blue-500 text-white p-2">1</div>
-      <div class="bg-green-500 text-white p-2">2</div>
-      <div class="bg-red-500 text-white p-2">3</div>
-    </div>
-
-</template>
