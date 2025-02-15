67c0118b14fdbfbb112cecf1917a8d197e0351a4
yoshidakenji <63270478+ohiaeni@users.noreply.github.com>
2025-02-15 10:09:58 +0900

    userStoreの実装クラスのAuthentication を各メソッドでインスタンス化するように変更

--- backend/web/src/main/java/com/memoblend/web/auth/UserStoreImpl.java
+++ backend/web/src/main/java/com/memoblend/web/auth/UserStoreImpl.java
@@ -6,38 +6,41 @@ import java.util.stream.Collectors;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import com.memoblend.applicationcore.auth.UserStore;
 
 /**
  * ログインしているユーザーの情報を保持するためのクラスです。
  */
+@Component
 public class UserStoreImpl implements UserStore {
 
-    private final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-    @Override
-    public String getLoginUserName() {
-        if (authentication != null) {
-            return authentication.getName();
-        }
-        return "";
+  @Override
+  public String getLoginUserName() {
+    final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    if (authentication != null) {
+      return authentication.getName();
     }
+    return "";
+  }
 
-    @Override
-    public List<String> getLoginUserRoles() {
-        if (authentication != null) {
-            return authentication.getAuthorities().stream().map(GrantedAuthority::getAuthority)
-                    .collect(Collectors.toList());
-        }
-        return new ArrayList<>();
+  @Override
+  public List<String> getLoginUserRoles() {
+    final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    if (authentication != null) {
+      return authentication.getAuthorities().stream().map(GrantedAuthority::getAuthority)
+          .collect(Collectors.toList());
     }
+    return new ArrayList<>();
+  }
 
-    @Override
-    public boolean isInRole(String role) {
-        if (authentication != null) {
-            return authentication.getAuthorities().stream().map(GrantedAuthority::getAuthority)
-                    .anyMatch(roles -> roles.equals(role));
-        }
-        return false;
+  @Override
+  public boolean isInRole(String role) {
+    final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    if (authentication != null) {
+      return authentication.getAuthorities().stream().map(GrantedAuthority::getAuthority)
+          .anyMatch(roles -> roles.equals(role));
     }
+    return false;
+  }
 }
