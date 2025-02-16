e62ae15f3091e24e644289d890a8a4abf922f501
yoshidakenji <63270478+ohiaeni@users.noreply.github.com>
2025-02-16 13:38:26 +0900

    トップページの導入

--- frontend/diary/src/App.vue
+++ frontend/diary/src/App.vue
@@ -1,86 +1,52 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue'
+import DiaryIcon from './components/atoms/DiaryIcon.vue';
+import HamburgerMenuIcon from './components/atoms/HamburgerMenuIcon.vue';
+import XMarkIcon from './components/atoms/XMarkIcon.vue';
+
+const navigation = [
+  { name: 'Diary', href: '/diary', current: true },
+  { name: 'test', href: '/test', current: false },
+]
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div class="relative flex h-16 items-center justify-between">
+        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+          <DisclosureButton
+            class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset">
+            <span class="absolute -inset-0.5" />
+            <span class="sr-only">Open main menu</span>
+            <HamburgerMenuIcon v-if="!open" class="block size-6" aria-hidden="true" />
+            <XMarkIcon v-else class="block size-6" aria-hidden="true" />
+          </DisclosureButton>
+        </div>
+        <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+          <div class="flex shrink-0 items-center">
+            <DiaryIcon class="block size-6 fill-white" />
+          </div>
+          <div class="hidden sm:ml-6 sm:block">
+            <div class="flex space-x-4">
+              <RouterLink v-for="(item, index) in navigation" :key="index" :to="item.href"
+                :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'rounded-md px-3 py-2 text-sm font-medium']">
+                {{ item.name }}
+              </RouterLink>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </header>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <DisclosurePanel class="sm:hidden">
+      <div class="space-y-1 px-2 pt-2 pb-3">
+        <RouterLink v-for="(item, index) in navigation" :key="index" :to="item.href"
+          :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'rounded-md px-3 py-2 text-sm font-medium']">
+          {{ item.name }}
+        </RouterLink>
+      </div>
+    </DisclosurePanel>
+  </Disclosure>
   <RouterView />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (width >= 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
