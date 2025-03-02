34ed87334f874646eb46aff35d192d50de8a73b6
yoshidakenji <63270478+ohiaeni@users.noreply.github.com>
2025-03-02 15:59:04 +0900

    日記の新規作成画面を修正

--- frontend/diary/src/views/diary/DiaryCreateView.vue
+++ frontend/diary/src/views/diary/DiaryCreateView.vue
@@ -18,7 +18,7 @@ const createDiaryAsync = async (diary: PostDiaryRequest) => {
 }
 </script>
 <template>
-  <form class="max-w-sm mx-auto">
+  <form class="max-w-sm mx-auto" @submit.prevent="createDiaryAsync(diary)">
     <div class="mb-5">
       <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">タイトル</label>
       <input type="text" id="title"
@@ -40,8 +40,9 @@ const createDiaryAsync = async (diary: PostDiaryRequest) => {
         required v-model="diary.date" />
     </div>
     <button type="submit"
-      class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      @click="createDiaryAsync(diary)">作成</button>
+      class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+      作成
+    </button>
   </form>
 
 </template>
