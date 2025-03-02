16c1beda544b31832726858bb84a389229c4b631
yoshidakenji <63270478+ohiaeni@users.noreply.github.com>
2025-03-02 14:21:49 +0900

    単件取得のサービスメソッドを追加

--- frontend/diary/src/services/diary/diary-service.ts
+++ frontend/diary/src/services/diary/diary-service.ts
@@ -5,3 +5,8 @@ export async function getDiaries(): Promise<GetDiariesResponse> {
   const response = await diaryApi.getDiaries();
   return response.data;
 }
+
+export async function getDiary(id: number) {
+  const response = await diaryApi.getDiary(id);
+  return response.data;
+}
