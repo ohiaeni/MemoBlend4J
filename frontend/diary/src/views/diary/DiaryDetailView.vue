9d66cbbd8fb7d84871a8b26dd4d2df3b47f059b5
yoshidakenji <63270478+ohiaeni@users.noreply.github.com>
2025-03-02 14:22:05 +0900

    単件取得画面の追加

---
+++ frontend/diary/src/views/diary/DiaryDetailView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { GetDiaryResponse } from '@/generated/api-client';
+import { getDiary } from '@/services/diary/diary-service';
+import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const id = Number(route.params.id);
+
+const diary = ref<GetDiaryResponse>({
+  content: '',
+  date: '',
+  id: id,
+  title: '',
+  userId: 0,
+});
+
+onMounted(async () => {
+  diary.value = await getDiary(id);
+});
+</script>
