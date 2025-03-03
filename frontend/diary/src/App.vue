fbe905dea258dc2c4f9f36b7f9a49fc056400f1e
yoshidakenji <63270478+ohiaeni@users.noreply.github.com>
2025-03-04 06:58:22 +0900

    ナビゲーションバーを修正

--- frontend/diary/src/App.vue
+++ frontend/diary/src/App.vue
@@ -4,11 +4,10 @@
 
 <template>
   <v-app>
-    <v-app-bar :elevation="24" rounded>
+    <v-app-bar rounded>
       <template v-slot:prepend>
         <v-app-bar-nav-icon></v-app-bar-nav-icon>
       </template>
-
       <v-app-bar-title>
         <RouterLink to="/">MemoBlend</RouterLink>
       </v-app-bar-title>
