44a3ca238e169cf0b20982d3db1d86f56defd58c
yoshidakenji <63270478+ohiaeni@users.noreply.github.com>
2025-03-04 19:32:10 +0900

    タイトルのスタイルを修正

--- frontend/diary/src/App.vue
+++ frontend/diary/src/App.vue
@@ -8,8 +8,8 @@
       <template v-slot:prepend>
         <v-app-bar-nav-icon></v-app-bar-nav-icon>
       </template>
-      <v-app-bar-title>
-        <RouterLink to="/">MemoBlend</RouterLink>
+      <v-app-bar-title bold>
+        <RouterLink to="/" class="font-weight-bold text-decoration-none text-black">MemoBlend</RouterLink>
       </v-app-bar-title>
     </v-app-bar>
     <v-main>
