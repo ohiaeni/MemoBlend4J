a488479155663cb65a39a8d95922fe12871810e0
yoshidakenji <63270478+ohiaeni@users.noreply.github.com>
2025-03-08 14:22:44 +0900

    プラグインとしてグローバルエラーハンドラを登録

--- frontend/diary/src/main.ts
+++ frontend/diary/src/main.ts
@@ -4,10 +4,10 @@ import '@mdi/font/css/materialdesignicons.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { createVuetify } from 'vuetify'
-
 import App from './App.vue'
-import router from './router'
+import { router } from './router'
 import { VDateInput } from 'vuetify/labs/VDateInput';
+import { globalErrorHandler } from './shared/error-handler/global-error-handler';
 
 const app = createApp(App)
 const vuetify = createVuetify({
@@ -19,5 +19,6 @@ const vuetify = createVuetify({
 app.use(createPinia())
 app.use(router)
 app.use(vuetify)
+app.use(globalErrorHandler)
 
 app.mount('#app')
