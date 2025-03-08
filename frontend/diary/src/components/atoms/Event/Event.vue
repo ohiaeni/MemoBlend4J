0fd808b69deee076adb4d053bb83ca21bd6755d2
yoshidakenji <63270478+ohiaeni@users.noreply.github.com>
2025-03-08 09:11:17 +0900

    イベントコンポーネントを追加

---
+++ frontend/diary/src/components/atoms/Event/Event.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import type { CalendarEvent } from '@/types';
+
+const props = defineProps<{
+  event: CalendarEvent;
+}>();
+</script>
+<template>
+  <v-chip class="props.event.color" variant="flat" size="small">
+    {{ props.event.title }}
+  </v-chip>
+</template>
