b9dc118985332f2caf9b83e9689323d8ca4b750b
yoshidakenji <63270478+ohiaeni@users.noreply.github.com>
2025-03-08 16:27:44 +0900

    ローディングスピナーコンポーネントを追加

---
+++ frontend/diary/src/components/organisms/LoadingSpinnerOverlay/LoadingSpinnerOverlay.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const props = defineProps<{
+  showLoading: boolean;
+}>();
+</script>
+<template>
+  <v-overlay v-model="props.showLoading" />
+  <div class="d-flex align-center justify-center fill-height">
+    <v-progress-circular :size="50" indeterminate />
+  </div>
+</template>
