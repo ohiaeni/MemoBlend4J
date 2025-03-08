7b39f368634ea295d87a53281096b34f41da996c
yoshidakenji <63270478+ohiaeni@users.noreply.github.com>
2025-03-08 14:52:51 +0900

    エラーページに情報を追加

--- frontend/diary/src/views/error/ErrorView.vue
+++ frontend/diary/src/views/error/ErrorView.vue
@@ -1,2 +1,11 @@
-<script setup lang="ts"></script>
-<template></template>
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+</script>
+<template>
+  <v-container>
+    <h1>エラーが発生しました。</h1>
+    <v-btn @click="router.push({ name: 'diaries' })">日記一覧に戻る</v-btn>
+  </v-container>
+</template>
