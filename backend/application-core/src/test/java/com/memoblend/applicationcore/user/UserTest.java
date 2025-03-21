97bf7571217f32f8d9cd9498b3c9522930ba316d
berry8848 <46173232+berry8848@users.noreply.github.com>
2025-03-21 23:47:32 +0900

    UT修正

--- backend/application-core/src/test/java/com/memoblend/applicationcore/user/UserTest.java
+++ backend/application-core/src/test/java/com/memoblend/applicationcore/user/UserTest.java
@@ -1,136 +1,73 @@
-// package com.memoblend.applicationcore.user;
+package com.memoblend.applicationcore.user;
 
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-// import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-// import org.junit.jupiter.api.BeforeEach;
-// import org.junit.jupiter.api.Test;
-// import org.junit.jupiter.api.extension.ExtendWith;
-// import org.springframework.test.context.junit.jupiter.SpringExtension;
-// import org.springframework.validation.BindException;
-// import org.springframework.validation.BindingResult;
-// import org.springframework.validation.Validator;
-// import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-// import com.memoblend.applicationcore.constant.ExceptionIdConstants;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import com.memoblend.applicationcore.constant.ExceptionIdConstants;
 
-// /**
-//  * ユーザーのドメインモデルのテストクラスです。
-//  */
-// @ExtendWith(SpringExtension.class)
-// public class UserTest {
-
-//   private User user;
-//   private BindingResult bindingResult;
-//   private Validator validator;
-
-//   @BeforeEach
-//   void setUp() {
-//     setUpUser();
-//     this.bindingResult = new BindException(user, "User");
-//     setUpValidator();
-//   }
+/**
+ * ユーザーのドメインモデルのテストクラスです。
+ */
+@ExtendWith(SpringExtension.class)
+public class UserTest {
   
-//   @Test
-//   public void testNoError_正常系_ユーザーを作成できた場合はtrueを返す() {
-//     // Act
-//     validator.validate(user, bindingResult);
-//     // Assert
-//     assertTrue(!bindingResult.hasErrors());
-//   }
+  @Test
+  public void testNoError_正常系_ユーザーを作成できる() {
+    // Act
+    assertDoesNotThrow(() -> new User(1L, "testName",false));
+  }
 
-//   @Test
-//   public void testIdSetZero_正常系_idが0() {
-//     // Arrange
-//     user.setId(0L);
-//     // Act
-//     validator.validate(user, bindingResult);
-//     // Assert
-//     assertTrue(!bindingResult.hasErrors());
-//   }
+  @Test
+  public void testIdSetZero_正常系_idが0() {
+    assertDoesNotThrow(() -> new User(0L, "testName",false));
+  }
 
-//   @Test
-//   public void testIdIsNegative_異常系_idが負の数() {
-//     // Arrange
-//     user.setId(-1L);
-//     // Act
-//     validator.validate(user, bindingResult);
-//     // Assert
-//     assertEquals("id", bindingResult.getFieldError().getField());
-//     assertEquals(ExceptionIdConstants.E_USER_ID_IS_NEGATIVE, bindingResult.getFieldError().getDefaultMessage());
-//   }
+  @Test
+  public void testIdIsNegative_異常系_idが負の数() {
+    IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+        () -> new User(-1L, "testName", false));
+    assertEquals(ExceptionIdConstants.E_USER_ID_IS_NEGATIVE, e.getMessage());
+  }
 
-//   @Test
-//   public void testNameIsNull_異常系_nameがnull() {
-//     // Arrange
-//     user.setName(null);
-//     // Act
-//     validator.validate(user, bindingResult);
-//     // Assert
-//     assertEquals("name", bindingResult.getFieldError().getField());
-//     assertEquals(ExceptionIdConstants.E_USER_NAME_IS_BLANK, bindingResult.getFieldError().getDefaultMessage());
-//   }
+  @Test
+  public void testNameIsNull_異常系_nameがnull() {
+    IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+        () -> new User(1L, null, false));
+    assertEquals(ExceptionIdConstants.E_USER_NAME_IS_NULL, e.getMessage());
+  }
   
-//   @Test
-//   public void testNameIsBlank_異常系_nameが空白() {
-//     // Arrange
-//     user.setName(" ");
-//     // Act
-//     validator.validate(user, bindingResult);
-//     // Assert
-//     assertEquals("name", bindingResult.getFieldError().getField());
-//     assertEquals(ExceptionIdConstants.E_USER_NAME_IS_BLANK, bindingResult.getFieldError().getDefaultMessage());
-//   }
+  @Test
+  public void testNameIsBlank_異常系_nameが空白() {
+    IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+        () -> new User(1L, " ", false));
+    assertEquals(ExceptionIdConstants.E_USER_NAME_IS_BLANK, e.getMessage()); 
+  }
 
-//   @Test
-//   public void testNameIsTooShort_異常系_nameが0文字() {
-//     // Arrange
-//     user.setName("");
-//     // Act
-//     validator.validate(user, bindingResult);
-//     // Assert
-//     assertEquals("name", bindingResult.getFieldError().getField());
-//     assertEquals(ExceptionIdConstants.E_USER_NAME_IS_BLANK, bindingResult.getFieldError().getDefaultMessage());
-//   }
+  @Test
+  public void testNameIsTooShort_異常系_nameが0文字() {
+    IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+        () -> new User(1L, "", false));
+    assertEquals(ExceptionIdConstants.E_USER_NAME_IS_BLANK, e.getMessage());
+  }
   
-//   @Test
-//   public void testNameIsTooLong_異常系_nameの文字数オーバー() {
-//     // Arrange
-//     user.setName("a".repeat(999));
-//     // Act
-//     validator.validate(user, bindingResult);
-//     // Assert
-//     assertEquals("name", bindingResult.getFieldError().getField());
-//     assertEquals(ExceptionIdConstants.E_USER_NAME_LENGTH_IS_OUT_OF_RANGE, 
-//         bindingResult.getFieldError().getDefaultMessage());
-//   }
+  @Test
+  public void testNameIsTooLong_異常系_nameの文字数オーバー() {
+    IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+        () -> new User(1L, "a".repeat(999), false));
+    assertEquals(ExceptionIdConstants.E_USER_NAME_LENGTH_IS_OUT_OF_RANGE, e.getMessage());
+  }
 
-//   @Test
-//   public void testIsDeletedIsFalse_正常系_isDeletedがfalse() {
-//     // Arrange
-//     user.setIsDeleted(false);
-//     // Act
-//     validator.validate(user, bindingResult);
-//     // Assert
-//     assertTrue(!bindingResult.hasErrors());
-//   }
+  @Test
+  public void testIsDeletedIsFalse_正常系_isDeletedがfalse() {
+    assertDoesNotThrow(() -> new User(1L, "testName",false));
+  }
 
-//   @Test
-//   public void testIsDeletedIsTrue_正常系_isDeletedがtrue() {
-//     // Arrange
-//     user.setIsDeleted(true);
-//     // Act
-//     validator.validate(user, bindingResult);
-//     // Assert
-//     assertTrue(!bindingResult.hasErrors());
-//   }
-
-//   private void setUpUser() {
-//     this.user = new User(1L, "testName", false);
-//   }
-  
-//   private void setUpValidator() {
-//     LocalValidatorFactoryBean factory = new LocalValidatorFactoryBean();
-//     factory.afterPropertiesSet();
-//     this.validator = factory;
-//   }
-// }
+  @Test
+  public void testIsDeletedIsTrue_正常系_isDeletedがtrue() {
+    assertDoesNotThrow(() -> new User(1L, "testName",true));
+  }
+}
