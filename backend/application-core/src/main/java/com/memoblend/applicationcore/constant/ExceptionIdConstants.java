5acfc1ccda55698419509b6d3fef16d71d4ce909
yoshidakenji <jitianxianer018@gmail.com>
2025-03-22 16:17:48 +0900

    例外IDの修正

--- backend/application-core/src/main/java/com/memoblend/applicationcore/constant/ExceptionIdConstants.java
+++ backend/application-core/src/main/java/com/memoblend/applicationcore/constant/ExceptionIdConstants.java
@@ -8,23 +8,20 @@ public class ExceptionIdConstants {
   /** 日記ID:{0}の日記が見つかりませんでした。 */
   public static final String E_DIARY_NOT_FOUND = "diaryNotFound";
 
+  /** 日記の{0}は必須です。 */
+  public static final String E_DIARY_FIELD_IS_REQUIRED = "diaryFieldIsRequired";
+
+  /** 日記の{0}は{1}~{2}の範囲で入力してください。 */
+  public static final String E_DIARY_VALUE_IS_OUT_OF_RANGE = "diaryValueIsOutOfRange";
+
   /** ユーザーID：{0}のユーザーが見つかりませんでした。 */
   public static final String E_USER_NOT_FOUND = "userNotFound";
 
-  /** ユーザーID：{0}は必須です。 */
-  public static final String E_USER_ID_IS_NULL = "userIdIsNull";
+  /** ユーザーの{0}は必須です。 */
+  public static final String E_USER_NAME_IS_REQUIRED = "userNameIsRequired";
 
-  /** ユーザーID：{0}は0以上の値にしてください。 */
-  public static final String E_USER_ID_IS_NEGATIVE = "userIdIsNegative";
-
-  /** ユーザー名：{0}は必須です。 */
-  public static final String E_USER_NAME_IS_NULL = "userNameIsNull";
-
-  /** ユーザー名：{0}は必須です。 */
-  public static final String E_USER_NAME_IS_BLANK = "userNameIsBlank";
-
-  /** ユーザー名：{0}は1～15文字の範囲で入力してください。*/
-  public static final String E_USER_NAME_LENGTH_IS_OUT_OF_RANGE = "userNameLengthIsOutOfRange";
+  /** ユーザーの{0}は{1}~{2}の範囲で入力してください。 */
+  public static final String E_USER_VALUE_IS_OUT_OF_RANGE = "userValueIsOutOfRange";
 
   /** {0} を実行する権限がありません。 */
   public static final String E_PERMISSION_DENIED = "permissionDenied";
