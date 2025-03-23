a2d7a4064700ee8090e805173217efd8f6aab7e8
yoshidakenji <jitianxianer018@gmail.com>
2025-03-23 10:40:09 +0900

    ルーティングを分割

--- frontend/diary/src/router/index.ts
+++ frontend/diary/src/router/index.ts
@@ -1,37 +1,13 @@
-import DiariesView from '@/views/diary/DiariesView.vue'
-import DiaryCreateView from '@/views/diary/DiaryCreateView.vue'
-import DiaryDetailView from '@/views/diary/DiaryDetailView.vue'
-import DiaryEditView from '@/views/diary/DiaryEditView.vue'
-import ErrorView from '@/views/error/ErrorView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import { diaryRoutes } from './diary'
+import { errorRoutes } from './error'
+import { authenticationRoutes } from './authentication'
 
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'diaries',
-      component: DiariesView,
-    },
-    {
-      path: '/detail/:id',
-      name: 'detail',
-      component: DiaryDetailView,
-    },
-    {
-      path: '/detail/:id/edit',
-      name: 'edit',
-      component: DiaryEditView,
-    },
-    {
-      path: '/create',
-      name: 'create',
-      component: DiaryCreateView,
-    },
-    {
-      path: '/error',
-      name: 'error',
-      component: ErrorView
-    }
+    ...diaryRoutes,
+    ...errorRoutes,
+    ...authenticationRoutes
   ],
 })
