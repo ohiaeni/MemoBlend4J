b07900e1973b5a542f1367ac46f80eba65369537
berry8848 <46173232+berry8848@users.noreply.github.com>
2025-03-23 16:54:20 +0900

    contentが含まれない例外処理追加

--- analysisapi/analysisapi/ai_processor/diary_analyzer.py
+++ analysisapi/analysisapi/ai_processor/diary_analyzer.py
@@ -1,5 +1,6 @@
 import os
 from openai import OpenAI
+from fastapi import HTTPException
 
 class DiaryAnalyzer:
   """
@@ -10,13 +11,16 @@ class DiaryAnalyzer:
     self.client = OpenAI()
     self.client.api_key = os.getenv("OPENAI_API_KEY")
 
-  def analyze(self, diary):
-    if "content" in diary:
+  def analyze(self, json_data: dict):
+    if "content" in json_data:
       response = self.client.chat.completions.create(
         model="gpt-3.5-turbo-0125",
         messages=[
           {"role": "system", "content": "あなたは、優秀なアシスタントです"},
-          {"role": "user", "content": "次の文章に対する感想を教えて。" + diary["content"]},],
+          {"role": "user", "content": "次の文章に対する感想を教えて。" + json_data["content"]},],
         max_tokens=100
       )
-    return response
+      return response
+    
+    else:
+      raise HTTPException(status_code=400, detail="日記データに 'content' キーが含まれていません")
